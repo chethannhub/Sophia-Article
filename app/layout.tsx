e581a76419dd8f32856cd8cea43693bce898bf42
C-hetha-N <chethannchethann80@gmail.com>
2024-10-07 07:35:21 +0530

    layout logo url removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <head>
-        <link rel="icon" href="Sophia\public\logo.svg" sizes="any" />
-      </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
